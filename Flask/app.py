e0c03ea6ba13e5d8843afc29b108df38d16fc60a
sealsea48 <141545758+sealsea48@users.noreply.github.com>
2023-08-17 13:24:43 +0300

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -9,18 +9,16 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Define the route for the homepage
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        start_url = request.form['start_url']
-        output_filename = 'combined-output.docx'
-        save_path = os.path.join(script_directory, output_filename)
-        process_multiple_pages(start_url, save_path)
-        return render_template('index.html', download_link=output_filename)
-    return render_template('index.html', download_link=None)
+
+if request.method == 'POST':
+    start_url = request.form['start_url']
+    output_filename = 'combined-output.docx'
+    save_path = os.path.join(script_directory, output_filename)
+    process_multiple_pages(start_url, save_path)
+    render_template('index.html', download_link=output_filename)
+render_template('index.html', download_link=None)
 
 @app.route('/download/<filename>')
-def download_file(filename):
-    return send_from_directory(script_directory, filename, as_attachment=True)
-
+send_from_directory(script_directory, filename, as_attachment=True)
 #if __name__ == '__main__':
  #   app.run(debug=True)
