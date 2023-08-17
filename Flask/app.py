5eb1bbc959f3bcdf70f63598617580cc07512a20
sealsea48 <141545758+sealsea48@users.noreply.github.com>
2023-08-17 13:39:22 +0300

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -9,16 +9,21 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Define the route for the homepage
 @app.route('/', methods=['GET', 'POST'])
+def homepage():
+    download_link = None  # Default value for the download link
 
-if request.method == 'POST':
-    start_url = request.form['start_url']
-    output_filename = 'combined-output.docx'
-    save_path = os.path.join(script_directory, output_filename)
-    process_multiple_pages(start_url, save_path)
-    render_template('index.html', download_link=output_filename)
-render_template('index.html', download_link=None)
+    if request.method == 'POST':
+        start_url = request.form['start_url']
+        output_filename = 'combined-output.docx'
+        save_path = os.path.join(script_directory, output_filename)
+        process_multiple_pages(start_url, save_path)
+        download_link = output_filename
+
+    return render_template('index.html', download_link=download_link)
 
 @app.route('/download/<filename>')
-send_from_directory(script_directory, filename, as_attachment=True)
-#if __name__ == '__main__':
- #   app.run(debug=True)
+def download(filename):
+    return send_from_directory(script_directory, filename, as_attachment=True)
+
+homepage()
+download(filename)
